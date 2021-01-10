a997a23fc150005469640ee0be94fc62b5a191a5
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2021-01-10 13:35:01 +0100

    Added map class

---
+++ src/Scenes/Game/Map.cpp
@@ -0,0 +1,171 @@
+#include "Map.hpp"
+
+Map::Map(Screen *screen, Grid *grid) : Scene(screen) {
+  EXIT_IF_TRUE(this->screen->width < grid->width ||
+                   this->screen->height < grid->height,
+               "The screen must have a minimum size of "
+                   << grid->width << "x" << grid->height);
+
+  EXIT_IF_TRUE(this->screen->width % grid->width != 0,
+               "The width of the screen must be divisible by" << grid->width);
+
+  EXIT_IF_TRUE(this->screen->width % grid->height != 0,
+               "The height of the screen must be divisible by " << grid->height)
+
+  this->grid = grid;
+  this->grids_per_width = this->screen->width / this->grid->width;
+  this->grids_per_height = this->screen->height / this->grid->height;
+}
+
+Map::~Map() {
+  if (this->grid) delete this->grid;
+}
+
+void Map::draw() {
+  this->draw_edges();
+  this->generate_solution_path();
+  this->replace_solution_path();
+  this->refresh();
+}
+
+void Map::generate_solution_path() {
+  // Place the spawn room in the top row
+  this->rooms.push_back({CORRIDOR_ROOM_CODE,
+                         ((rand() % this->grids_per_width) * this->grid->width) + 1,
+                         1});
+
+  // Print the spawn type room to the screen
+  mvwaddch(this->screen->window, this->rooms[0].start_y, this->rooms[0].start_x, this->rooms[0].type);
+
+  // Keeps track of the last room placed
+  // (at the beginning it points to the spawn room)
+  Room *last_room = &this->rooms[0];
+
+  while (true) {
+    // Make a copy of the last room placed
+    Room *next_room = new Room(CORRIDOR_ROOM_CODE,
+                               last_room->start_x,
+                               last_room->start_y);
+
+    const std::string *moves = get_available_moves(next_room);
+    const int move_idx = rand() % moves->size();
+    const char move_ch = moves->at(move_idx);
+    delete moves;
+
+    // The solution path moves right
+    if (move_ch == 'R') next_room->start_x += this->grid->width;
+
+    // The solution path moves left
+    if (move_ch == 'L') next_room->start_x -= this->grid->width;
+
+    // The solution path moves down
+    if (move_ch == 'D') {
+      next_room->start_y += this->grid->height;
+
+      // If the new ordinate exceed the screen height,
+      // draw the exit and stop the generation
+      if (next_room->start_y >= this->screen->height) {
+        delete next_room;
+        break;
+      }
+
+      // Update the last room to be type 2 and print it to the screen
+      this->rooms.back().type = DROP_ROOM_CODE;
+      mvwaddch(this->screen->window, last_room->start_y, last_room->start_x, last_room->type);
+
+      // The next room MUST be type 3
+      next_room->type = LANDING_ROOM_CODE;
+    }
+
+    // 1) Print to the screen the next_room
+    // 2) Store the next room in the rooms vector
+    // 3) Update the last room pointer
+    // 4) Free the memory of the next_room
+    mvwaddch(this->screen->window, next_room->start_y, next_room->start_x, next_room->type);
+    this->rooms.push_back(*next_room);
+    last_room = &this->rooms.back();
+    delete next_room;
+  }
+
+  // If there are two consecutive rooms of type 2,
+  // the type of the second one is replaced with 4
+  for (auto &current_room : this->rooms) {
+    auto next_room = *(&current_room + 1);
+
+    if ((current_room.type == DROP_ROOM_CODE ||
+         current_room.type == OPEN_ROOM_CODE) &&
+        next_room.type == DROP_ROOM_CODE) {
+      next_room.type = OPEN_ROOM_CODE;
+      mvwaddch(this->screen->window, next_room.start_y, next_room.start_x, next_room.type);
+    }
+  }
+
+#if DEBUG == 1
+  endwin();
+  for (auto &room : this->rooms)
+    std::cout << room << std::endl;
+#endif
+}
+
+std::string *Map::get_available_moves(Room *last_room) {
+  std::string *moves = new std::string("D");
+
+  if (can_move(last_room->start_x - this->grid->width, last_room->start_y))
+    moves->append(1, 'L');
+
+  if (can_move(last_room->start_x + this->grid->width, last_room->start_y))
+    moves->append(1, 'R');
+
+  return moves;
+}
+
+const bool Map::can_move(int x, int y) {
+  if (x <= 0 || y <= 0) return false;
+
+  const char ch = mvwinch(this->screen->window, y, x) & A_CHARTEXT;
+
+  return ch == ' ';
+}
+
+void Map::replace_solution_path() {
+  for (int i = 0; i < this->grids_per_height; ++i) {
+    for (int j = 0; j < this->grids_per_width; ++j) {
+      int start_x = (j * this->grid->width) + 1;
+      int start_y = (i * this->grid->height) + 1;
+
+      const char room_type = mvwinch(this->screen->window, start_y, start_x) & A_CHARTEXT;
+      const std::vector<std::string> *rooms = nullptr;
+
+      if (room_type == CORRIDOR_ROOM_CODE) rooms = &CORRIDOR_ROOMS;
+      if (room_type == DROP_ROOM_CODE) rooms = &DROP_ROOMS;
+      if (room_type == LANDING_ROOM_CODE) rooms = &LANDING_ROOMS;
+      if (room_type == OPEN_ROOM_CODE) rooms = &OPEN_ROOMS;
+
+      this->add_room(start_x, start_y, rooms);
+    }
+  }
+}
+
+void Map::add_room(int start_x, int start_y, const std::vector<std::string> *rooms) {
+  const std::string *room = nullptr;
+  unsigned int counter_room = 0;
+  if (rooms && !rooms->empty()) {
+    int idx_rooms = rand() % rooms->size();
+    room = &rooms->at(idx_rooms);
+  }
+
+  for (int i = start_y; i < start_y + this->grid->height; ++i) {
+    for (int j = start_x; j < start_x + this->grid->width; ++j) {
+      char ch = 'X';
+
+      if (room && counter_room < room->size()) {
+        ch = room->at(counter_room++);
+
+        if (ch == '0') ch = ' ';
+        if (ch == '1') ch = '=';
+      }
+
+      mvwaddch(this->screen->window, i, j, ch);
+    }
+  }
+}
---
+++ src/Scenes/Game/Map.hpp
@@ -0,0 +1,87 @@
+#ifndef MAP_HPP
+#define MAP_HPP
+
+#include <stdlib.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../../constants.hpp"
+#include "../Scene.hpp"
+
+#define DEBUG 1
+#define EXIT_IF_TRUE(expression, msg) \
+  {                                   \
+    if (expression) {                 \
+      endwin();                       \
+      std::cout << msg << std::endl;  \
+      exit(EXIT_FAILURE);             \
+    }                                 \
+  }
+
+struct Grid {
+  int width,
+      height;
+
+  Grid(int width,
+       int height)
+      : width(width),
+        height(height) {}
+};
+
+struct Room {
+  char type;
+  int start_x;
+  int start_y;
+
+  Room(char type, int start_x, int start_y)
+      : type(type),
+        start_x(start_x),
+        start_y(start_y) {}
+
+  Room(const Room &room)
+      : type(room.type),
+        start_x(room.start_x),
+        start_y(room.start_y) {}
+};
+
+#if DEBUG == 1
+inline std::ostream &operator<<(std::ostream &os, const Room &room) {
+  os << "(" << room.start_x << ", " << room.start_y << ") " << room.type;
+  return os;
+}
+#endif
+
+class Map : public Scene {
+  // It as a left and right exit
+  static const char CORRIDOR_ROOM_CODE = '1';
+
+  // It has a left, right and bottom exit
+  static const char DROP_ROOM_CODE = '2';
+
+  // It has a left, right and top exit
+  static const char LANDING_ROOM_CODE = '3';
+
+  // It has an exit on all four sides
+  static const char OPEN_ROOM_CODE = '4';
+
+ private:
+  Grid *grid;
+  std::vector<Room> rooms;
+  int grids_per_width;
+  int grids_per_height;
+
+  void generate_solution_path();
+  std::string *get_available_moves(Room *next_room);
+  const bool can_move(int x, int y);
+  void replace_solution_path();
+  void add_room(int x, int y, const std::vector<std::string> *rooms);
+
+ public:
+  Map(Screen *screen, Grid *grid);
+  ~Map();
+  void draw();
+};
+
+#endif
